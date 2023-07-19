8fe3fd92582efecee06ac080eaecee770a1b0825
CrystalXue0626 <137978133+CrystalXue0626@users.noreply.github.com>
2023-07-20 03:59:12 +1000

    Update newnuscenes.py

--- openpifpaf_newnuscenes/newnuscenes.py
+++ openpifpaf_newnuscenes/newnuscenes.py
@@ -28,10 +28,10 @@ class newNuScenes(openpifpaf.datasets.DataModule):
     debug = False
     pin_memory = False
 
-    train_annotations = '/content/mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
-    val_annotations = '/content/mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
-    eval_annotations = '/content/mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
-    train_image_dir = '/content/mmdetection3d/data/nuimages/annotations/semantic_masks/samples'
+    train_annotations = 'mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
+    val_annotations = 'mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
+    eval_annotations = 'mmdetection3d/data/nuimages/annotations/nuimages_v1.0-mini.json'
+    train_image_dir = 'mmdetection3d/data/nuimages/annotations/semantic_masks/samples'
     val_image_dir = train_image_dir    # All images in the the same dir --> annotations
     eval_image_dir = train_image_dir   # are in such a way that the correct file is chosen
 
